e576533e4e8db59bfcf56be8760ef62d20018fc0
Rupesh <rupesh.official484@gmail.com>
2025-01-05 01:39:12 +0530

    change!!

--- app/src/main/java/com/warrantysafe/app/di/modules/DomainModule.kt
+++ app/src/main/java/com/warrantysafe/app/di/modules/DomainModule.kt
@@ -1,6 +1,8 @@
 package com.warrantysafe.app.di.modules
 
 import com.warrantysafe.app.domain.useCases.AddProductUseCase
+import com.warrantysafe.app.domain.useCases.GetActiveProductsUseCase
+import com.warrantysafe.app.domain.useCases.GetExpiredProductsUseCase
 import com.warrantysafe.app.domain.useCases.GetNotificationsUseCase
 import com.warrantysafe.app.domain.useCases.GetProductsUseCase
 import org.koin.dsl.module
@@ -8,6 +10,8 @@ import org.koin.dsl.module
 val domainModule = module {
     // Provide Use Cases
     single { GetProductsUseCase(get()) } // Add domain layer dependencies here, e.g., use cases
+    single { GetActiveProductsUseCase(get()) } // Add domain layer dependencies here, e.g., use cases
+    single { GetExpiredProductsUseCase(get()) } // Add domain layer dependencies here, e.g., use cases
     single { GetNotificationsUseCase(get()) } // Add domain layer dependencies here, e.g., use cases
     single { AddProductUseCase(get()) }
 }
