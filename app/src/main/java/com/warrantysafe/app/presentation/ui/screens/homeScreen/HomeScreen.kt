0473b448242acff2ef173651f64b4179dbc44482
Rupesh <rupesh.official484@gmail.com>
2025-01-08 15:07:15 +0530

    re-enabled gestures!!

--- app/src/main/java/com/warrantysafe/app/presentation/ui/screens/homeScreen/HomeScreen.kt
+++ app/src/main/java/com/warrantysafe/app/presentation/ui/screens/homeScreen/HomeScreen.kt
@@ -46,11 +46,9 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import com.warrantysafe.app.R
 import com.warrantysafe.app.presentation.navigation.Route
 import com.warrantysafe.app.presentation.ui.screens.homeScreen.tabs.ActiveTab
